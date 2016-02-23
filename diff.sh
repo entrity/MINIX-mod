4fdd6ef7e231c7ead66f8ef8f02b88438cafacb1
Markham <quinesquines@gmail.com>
2016-02-23 10:14:19 -0800

    sh

--- diff.sh
+++ diff.sh
@@ -1,2 +1,2 @@
 INITIAL_COMMIT=bc9bcff4c56599645728fa4da9895846eeb37301
-git diff $INITIAL_COMMIT -- kernel include boot commands drivers etc lib servers tools
+git diff -w $INITIAL_COMMIT -- kernel include boot commands drivers etc lib servers tools
